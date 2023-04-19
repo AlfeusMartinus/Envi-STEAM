641f251c9f9fc86bfae12e1dc7ab1b22afe1b457
Alfeus Martinus Sinurat <feusmartinus@gmail.com>
2023-04-19 17:50:12 +0700

    add material and add quiz for activity 3

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ document.querySelectorAll('a[href^="#course"]').forEach((anchor) => {
     });
   });
 });
+
+var myList = document.getElementById("my-list");
+var startValue = 4;
+var listItem = myList.getElementsByTagName("li");
+for (var i = 0; i < listItem.length; i++) {
+  listItem[i].setAttribute("value", startValue++);
+}
